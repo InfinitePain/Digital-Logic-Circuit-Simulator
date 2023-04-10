b708c739df8a54d66569ab273154c7b226192007
Emirhan Göktas <emirhangoktas01@gmail.com>
2023-04-10 17:42:42 +0200

    Implement Xor2 class

--- src/xor2.h
+++ src/xor2.h
@@ -7,18 +7,29 @@
  ============================================================================
  */
 
-#include <iostream>
-#include <cstdlib>
-#include "baustein.h"
-#include "schnittstelle.h"
+#pragma once
 
-class Xor2 {
+#include "baustein.h"
+#include <vector>
+
+class Xor2: public Baustein {
 public:
 	Xor2(Schnittstelle* e0, Schnittstelle* e1, Schnittstelle* a0) {
-		// code
+		addEingang(e0);
+		addEingang(e1);
+		addAusgang(a0);
+		name = "Xor2";
 	}
 
 	void update() {
-		// code
+		short e0Pegel = eingaenge.at(0)->getPegel();
+		short e1Pegel = eingaenge.at(1)->getPegel();
+
+		if (e0Pegel == Schnittstelle::UNDEFINED || e1Pegel == Schnittstelle::UNDEFINED) {
+			ausgaenge.at(0)->setPegel(Schnittstelle::UNDEFINED);
+		}
+		else {
+			ausgaenge.at(0)->setPegel(e0Pegel ^ e1Pegel);
+		}
 	}
 };
