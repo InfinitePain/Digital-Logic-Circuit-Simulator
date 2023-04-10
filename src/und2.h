44a9456d3c13801e61f1b77302847d63b871baf1
Emirhan Göktas <emirhangoktas01@gmail.com>
2023-04-10 17:42:30 +0200

    Implement Und2 class

--- src/und2.h
+++ src/und2.h
@@ -7,18 +7,29 @@
  ============================================================================
  */
 
-#include <iostream>
-#include <cstdlib>
-#include "baustein.h"
-#include "schnittstelle.h"
+#pragma once
 
-class Und2 {
+#include "baustein.h"
+#include <vector>
+
+class Und2: public Baustein{
 public:
 	Und2(Schnittstelle* e0, Schnittstelle* e1, Schnittstelle* a0) {
-		// code
+		addEingang(e0);
+		addEingang(e1);
+		addAusgang(a0);
+		name = "Und2";
 	}
 
 	void update() {
-		// code
+		short e0Pegel = eingaenge.at(0)->getPegel();
+		short e1Pegel = eingaenge.at(1)->getPegel();
+
+		if (e0Pegel == Schnittstelle::UNDEFINED || e1Pegel == Schnittstelle::UNDEFINED) {
+			ausgaenge.at(0)->setPegel(Schnittstelle::UNDEFINED);
+		}
+		else {
+			ausgaenge.at(0)->setPegel(e0Pegel & e1Pegel);
+		}
 	}
 };
